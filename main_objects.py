56115500a14c8496aa0c120e7e4c090d6d878099
Filip Nechansky <filip.nechansky@cern.ch>
2019-12-29 17:52:11 +0100

    First complete "story"

--- main_objects.py
+++ main_objects.py
@@ -1,5 +1,4 @@
 
-possibleDirections = ["north","east","south","west"]
 
 def init(what,where):
 	if what in where:
@@ -9,57 +8,118 @@ def init(what,where):
 
 class exitClass:
 
-    def __init__(self,properties):
-        self.description = properties["Description"]
+    def __init__(self,name,properties):
+        self.direction = name
+        self.look = properties["l"]
+        self.description = properties["x"]
         self.destination = properties["Destination"]
         self.name = properties["Name"]
-        self.isLocked    = False	
+        self.items = {}
+
+        self.isLocked    = False    
         if "Locked" in properties:
             self.isLocked = True
             self.lockItem = properties["Locked"][0]
             self.lockDescription = properties["Locked"][1]
             self.unlockMessageSuccess = properties["Locked"][2]
             self.unlockMessageFail = properties["Locked"][3]
+            self.unlockedDescription = properties["Locked"][4]
+
+    def describe(self):
+        out = "  "
+        out += self.description
+        print(out)
 
     def  use(self,name):
         if name == self.lockItem:
-            print(self.unlockMessageSuccess)
+            print("  "+self.unlockMessageSuccess)
             self.isLocked = False
+            self.description = self.unlockedDescription
         else:
-            print(self.unlockMessageFail)
+            print("  "+self.unlockMessageFail)
 
     def exit(self):
         if self.isLocked:
-            print(self.lockDescription)
+            print("  "+self.lockDescription)
             return "Locked"
         else:
             return self.destination
 
 class itemClass:
 
-	def __init__(self,properties):
-		self.look = init("l",properties)
-		self.description = init("x",properties)
-		self.take = init("l",properties)
-		self.move = False
-		self.moveDescription = "Unmovable"
-		if "m" in properties:
-			self.move = properties["m"][0]
-			self.moveDescription = properties["m"][1]
+    def __init__(self,name,properties):
+        self.name = name
+        self.look = init("l",properties)
+        self.description = init("x",properties)
+        self.move = False
+        self.moveDescription = "You cannot move " + name + " or moving it won't do anything"
+
+        # what is in the object
+        self.items = {}
+        if "Items" in properties:
+            for item in properties["Items"]:
+                self.items[item] = itemClass(item,properties["Items"][item])
+
+        if "m" in properties:
+            self.move = properties["m"][0]
+            self.moveDescription = properties["m"][1]
+
+        self.take = False
+        self.takeDescription = "You cannot take " + name
+
+        if "t" in properties:
+            self.take = properties["t"][0]
+            self.takeDescription = properties["t"][1]
+
+        self.isLocked    = False    
+        if "Locked" in properties:
+            self.isLocked = True
+            self.lockItem = properties["Locked"][0]
+            self.lockDescription = properties["Locked"][1]
+            self.unlockMessageSuccess = properties["Locked"][2]
+            self.unlockMessageFail = properties["Locked"][3]
+            self.unlockedDescription = properties["Locked"][4]
+
+    def describe(self):
+        out = "  "
+        out += self.description
+
+        if not self.isLocked and len(self.items):
+            out += "\n\n"
+            out += "  "
+            for it in self.items:
+                out += self.items[it].look + " "
+        print(out)
+
+    def delete(self,name):
+        if name in self.items:
+            del self.items[name]
+            for nm,it in self.items.items():
+                it.delete(name)
+
+    def  use(self,name):
+        if name == self.lockItem:
+            print("  "+self.unlockMessageSuccess)
+            self.isLocked = False
+            self.description = self.unlockedDescription
+        else:
+            print("  "+self.unlockMessageFail)
 
 class roomClass:
+    possibleDirections = ["north","east","south","west"]
 
     def __init__(self,properties):
         self.title = properties["Title"]
         self.description = properties["Description"]
         # what is in the room
         self.items = {}
-        for item in properties["Items"]: # TODO add check for existemce
-        	self.items[item] = itemClass(properties["Items"][item])
+        if "Items" in properties:
+            for item in properties["Items"]:
+                self.items[item] = itemClass(item,properties["Items"][item])
         # what are the exits fro the room (name)
         self.exits = {}
         for exit in properties["Exits"]: # TODO add check for existemce
-        	self.exits[exit] = exitClass(properties["Exits"][exit])
+        	self.exits[exit] = exitClass(exit,properties["Exits"][exit])
         # was this room visited before?
         self.visited = False
 
@@ -73,8 +133,18 @@ class roomClass:
         out += "\n\n"
         out += "  "
         for ex in self.exits:
-            out += self.exits[ex].description + " "
+            out += self.exits[ex].look + " "
         print(out)
+
+    def describe_object(self,name):
+        obj =  self.get_object(name)
+        if obj == "NONE":
+            return "NONE"
+        obj.describe()
+        if not obj.isLocked:
+            for it in obj.items:
+                nameIt = obj.items[it].name
+                self.items[nameIt] = obj.items[it]
 
     def enter(self):
         print(self.title)                                            
@@ -91,22 +161,30 @@ class roomClass:
             ex = self.exits[exDir]
             if ex.name == name:
                 return ex
+        print("  No object " + name + " in the room.")
         return "NONE"
 
     def take_object(self,name):
         if name in self.items:
             content = self.items[name]
-            del self.items[name]
+            print("  " + content.takeDescription)
+            if content.take:
+                del self.items[name]
+                for nm,it in self.items.items():
+                    it.delete(name)
+                return content
+        else:
+            print("  No object " + name + " in the room.")
         return "NONE"
 
     def get_exit(self,direction):
         direction = direction.lower()
         
         if not direction in possibleDirections:
-            print(direction + " is not a valid direction")
+            print("  " + direction + " is not a valid direction")
             return "err"
 
         if direction in self.exits:
             return self.exits[direction].exit()
         else:
-            print("Cannot go " + direction)
+            print("  Cannot go " + direction)
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from main_objects import roomClass
 from main_objects import itemClass
 from main_objects import exitClass
@@ -18,6 +20,8 @@ except:
 jsonData = {}
 rooms = {}
 inventory = {}
+
+yes = ["yes","YES","Yes","y","Y"]
 
 def read_json( fileName ):
     global jsonData 
@@ -44,7 +48,12 @@ def eval_input():
 
     verb = inp[0]
     if verb == "exit":
-        return True
+        confirmation = input("\n  Are you sure? ")
+        if confirmation in yes:
+            return True
+        else:
+            print("\n  I did not understand...")
+            return False
 
     print("")
 
@@ -59,42 +68,69 @@ def eval_input():
             else:
                 curRoom = rooms[res]
                 curRoom.enter()
-
+                if res == "end":
+                    print("\n\n--------------------------------------")
+                    print("You have escaped! Congratulations!")
+                    return True
     elif verb == "use":
-        if length == 1:
-            print("Use what on what?")
+        if length < 2:
+            print("  Use what on what?")
             return False
         item = inp[1]
         if item not in inventory:
-            print("You do not have " + item + " in inventory.")
+            print("  You do not have " + item + " in inventory.")
             return False
-        if length == 2:
-            print("Use " + item + " on what?")
+        if length < 3:
+            print("  Use " + item + " on what?")
             return False
-        obj = curRoom.get_object(inp[2])
+        pos = 2
+        if inp[2] == "on": 
+            if length < 4:
+                print("  Use " + item + " on what?")
+                return False
+            pos = 3
+        obj = curRoom.get_object(inp[pos])
         if obj == "NONE":
-            print("No object" + inp[2] + "in the room.")
             return False
         obj.use(item)
     elif verb == "look":
         curRoom.describe()
     elif verb == "examine":
         if length == 1:
-            print("Examine what?")
+            print("  Examine what?")
             return False
-        obj = curRoom.get_object(inp[1])
-        if obj == "NONE":
-            print("No object" + inp[1] + "in the room")
-            return False
-        print(obj.description)
+        if inp[1] in inventory:
+            inventory[inp[1]].describe()
+            return False 
+        curRoom.describe_object(inp[1])
     elif verb == "take":
         obj = curRoom.take_object(inp[1])
         if obj == "NONE":
-            print("No object" + inp[1] + "in the room")
             return False
         inventory[inp[1]] = obj
-    print("Unknown verb "+verb)
+    elif verb == "inventory":
+        if len(inventory)==0: 
+            print("  You have nothing! NOTHING!!!")
+            return False
+        out = "  You have: "
+        for item in inventory:
+            out += item + ", "
+        print(out)
+    elif verb == "help":
+        print("Possible commands:")
+        print(" * go (where), possible directions:")
+        print(" * exit, quits the game")
+        print(roomClass.possibleDirections)
+        print(" * use (what) [on] (what)")
+        print(" * look, describes current room")
+        print(" * examine (object), examines object or item in current room or inventory")
+        print(" * take (object), takes object and saves to inventory")
+        print(" * inventory, lists items in your inventory")
+    else: 
+        print("  Unknown verb " + verb)
     return False
+
+print("\nYou have awoken in a foreign room. Find a way to escape!\n\n")
 
 load_stuff()
 
@@ -103,15 +139,3 @@ curRoom.enter()
 
 while True:
 	if eval_input(): break
-
-
-#res = curRoom.get_exit("north")
-#print(res)
-#
-#curRoom.exits["north"].use("key")
-#
-#res = curRoom.get_exit("north")
-#print(res)
-#
-#curRoom = rooms[res]
-#curRoom.enter()
